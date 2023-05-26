9910843b22d3bec88d6a0066f3a3bcc19a75d284
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-26 19:55:45 -0300

    FINALIZADO

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Made By Silktz 
+
+string ponto;
+
+Console.WriteLine("Fluxograma de Engenharia");
+Console.WriteLine("Use o <S> para 'SIM', ou outro para 'NÃO' <N>");
+
+Console.WriteLine("O objeto esta se movendo? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0,1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0,1).ToUpper() == "S";
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+     ponto = "Otimo, Sem Problemas.";
+    }
+    else
+    {
+     ponto = "use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+     ponto = "otimo!";
+    }
+    else
+    {
+     ponto = "use silver tape.";
+    }
+}
+Console.WriteLine(ponto);
+Console.Clear();
